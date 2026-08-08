934b128899883c3ca85b6bc551ded7c0f322676a
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2026-08-08 13:44:40 +0530

    updated css

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-
 export default function ExpenseForm() {
   const [expenses, setExpenses] = useState(() => {
     const storedExpenses = localStorage.getItem("expenses");
@@ -144,124 +143,143 @@ export default function ExpenseForm() {
   };
   return (
     <>
-    <h1>Expense tracker</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="formField">
-          <label htmlFor="expensename"> Expense Name</label>
-          <input
-            type="text"
-            name="expensename"
-            value={formData.name}
-            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-          />
-        </div>
-        <p>{error.name}</p>
-        <div className="formField">
-          <label htmlFor="amount"> Amount</label>
-          <input
-            type="number"
-            name="amount"
-            value={formData.amount}
-            onChange={(e) =>
-              setFormData({ ...formData, amount: e.target.value })
-            }
-          />
-        </div>
-        <p>{error.amount}</p>
-        <div className="formField">
-          <label htmlFor="category"> Amount</label>
-          <select
-            name="category"
-            id=""
-            value={formData.category}
-            onChange={(e) =>
-              setFormData({ ...formData, category: e.target.value })
-            }
-          >
-            <option value="">Select Category</option>
-            <option value="food">food</option>
-            <option value="shopping">Shopping</option>
-            <option value="travel">Travel</option>
-          </select>
-        </div>
-        <p>{error.category}</p>
-        <div className="formField">
-          <label htmlFor="date"> Amount</label>
-          <input
-            type="date"
-            name="date"
-            value={formData.date}
-            onChange={(e) => setFormData({ ...formData, date: e.target.value })}
-          />
-        </div>
-        <p>{error.date}</p>
-        {isUpdate.isUpdate ? (
-          <button
-            type="button"
-            onClick={(e) => updateExpense(isUpdate.expenseId)}
-          >
-            Update Expense
-          </button>
-        ) : (
-          <button>Add Expense</button>
-        )}
-      </form>
-      {filteredExpenses.map((expense, index) => (
-        <div key={index} className="expenseContainer">
-          <div className="expenseName">
-            Expense: <span> {expense.name}</span>
+      <div className="container flex flex-col justify-center items-center gap-8 m-auto pt-10">
+        <h1 className="text-4xl font-bold">Expense tracker</h1>
+        <div className="expenseDetails flex gap-5">
+          <div className="total p-3 border-1 text-center w-50">
+            <h3 className="font-bold">TOTAL EXPENSE </h3>
+            <p>
+              ₹
+              {filteredExpenses.reduce((total, filteredExpense) => {
+                return total + Number(filteredExpense.amount);
+              }, 0)}
+            </p>
           </div>
-          <div className="expenseAmount">
-            Amount: <span> {expense.amount}</span>
+          <div className="transactionCount p-3 border-1 text-center w-50">
+            <h3 className="font-bold">TRANSACTIONS </h3>
+            <p></p>
           </div>
-          <div className="expenseCategory">
-            Category: <span> {expense.category}</span>
+          <div className="avgExp p-3 border-1 text-center w-50">
+            <h3 className="font-bold">AVERAGE EXPENSE </h3>
+            <p></p>
           </div>
-          <div className="expenseDate">
-            Date: <span> {expense.date}</span>
-          </div>
-          <button
-            onClick={() => {
-              deleteExpense(index);
-            }}
-          >
-            Delete
-          </button>
-          <button
-            onClick={() => {
-              // console.log(index);
-              editExpense(index);
-            }}
-          >
-            Edit
-          </button>
         </div>
-      ))}
-      {expenses.length > 0 ? (
-        <div>
-          <div>
+        <form onSubmit={handleSubmit}>
+          <div className="formField">
+            <label htmlFor="expensename"> Expense Name</label>
+            <input
+              type="text"
+              name="expensename"
+              value={formData.name}
+              onChange={(e) =>
+                setFormData({ ...formData, name: e.target.value })
+              }
+            />
+          </div>
+          <p className="error">{error.name}</p>
+          <div className="formField">
+            <label htmlFor="amount"> Amount</label>
+            <input
+              type="number"
+              name="amount"
+              value={formData.amount}
+              className="border-grey-400"
+              onChange={(e) =>
+                setFormData({ ...formData, amount: e.target.value })
+              }
+            />
+          </div>
+          <p className="error">{error.amount}</p>
+          <div className="formField">
+            <label htmlFor="category"> Amount</label>
             <select
-              name="filter"
-              id="filter"
-              onChange={(e) => filter(e.target.value)}
+              name="category"
+              id=""
+              value={formData.category}
+              onChange={(e) =>
+                setFormData({ ...formData, category: e.target.value })
+              }
             >
-              <option value="">Filter By</option>
-              <option value="food">Food</option>
-              <option value="shopping">shopping</option>
+              <option value="">Select Category</option>
+              <option value="food">food</option>
+              <option value="shopping">Shopping</option>
               <option value="travel">Travel</option>
             </select>
           </div>
-          <div>
-            {" "}
-            Total: ₹{" "}
-            {filteredExpenses.reduce((total, filteredExpense) => {
-              return total + Number(filteredExpense.amount);
-            }, 0)}
+          <p className="error">{error.category}</p>
+          <div className="formField">
+            <label htmlFor="date"> Amount</label>
+            <input
+              type="date"
+              name="date"
+              value={formData.date}
+              onChange={(e) =>
+                setFormData({ ...formData, date: e.target.value })
+              }
+            />
           </div>
-        </div>
-      ) : (
-        ""
-      )}
+          <p className="error">{error.date}</p>
+          {isUpdate.isUpdate ? (
+            <button
+              type="button"
+              onClick={(e) => updateExpense(isUpdate.expenseId)}
+            >
+              Update Expense
+            </button>
+          ) : (
+            <button>Add Expense</button>
+          )}
+        </form>
+        {filteredExpenses.map((expense, index) => (
+          <div key={index} className="expenseContainer">
+            <div className="expenseName">
+              Expense: <span> {expense.name}</span>
+            </div>
+            <div className="expenseAmount">
+              Amount: <span> {expense.amount}</span>
+            </div>
+            <div className="expenseCategory">
+              Category: <span> {expense.category}</span>
+            </div>
+            <div className="expenseDate">
+              Date: <span> {expense.date}</span>
+            </div>
+            <button
+              onClick={() => {
+                deleteExpense(index);
+              }}
+            >
+              Delete
+            </button>
+            <button
+              onClick={() => {
+                // console.log(index);
+                editExpense(index);
+              }}
+            >
+              Edit
+            </button>
+          </div>
+        ))}
+        {expenses.length > 0 ? (
+          <div>
+            <div>
+              <select
+                name="filter"
+                id="filter"
+                onChange={(e) => filter(e.target.value)}
+              >
+                <option value="">Filter By</option>
+                <option value="food">Food</option>
+                <option value="shopping">shopping</option>
+                <option value="travel">Travel</option>
+              </select>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
     </>
   );
 }
